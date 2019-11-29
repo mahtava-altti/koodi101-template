50080c698683d64ec87d5b34133367dd53047857
mahtava-altti <mathava-altti@protonmail.com>
2019-11-29 11:27:14 +0100

    Now the font changes color depending on the background's color

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -44,6 +44,28 @@ class App extends Component {
     const response = await getGreetingFromBackend();
     this.setState({data: response.results[response.results.length - 1]});
     document.body.style.backgroundColor='rgb('+this.state.data.red.toString()+','+this.state.data.green.toString()+','+this.state.data.blue.toString()+')';
+    
+    var rgb = [this.state.data.red, this.state.data.green, this.state.data.blue];
+
+    for(let i=0; i<rgb.length; i++) {
+	var c = rgb[i]
+	c = c / 255.0;
+	if(c <= 0.03928){
+		c = c/12.92;
+	} else {
+	 	c = ((c+0.055)/1.055)**2.4;
+	}
+	rgb[i] = c;
+    }
+
+    const L = 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2];
+
+    if(L > 0.179){
+    	document.body.style.color='black';
+    } else{
+    	document.body.style.color='white';
+    }
+    
   }
 
   render() {
